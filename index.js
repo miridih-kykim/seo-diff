eb9aec4fb50eea91910d9549419798e1b5999f30
miridih-jwlee01 <jwlee01@miridih.com>
2024-06-12 10:52:58 +0900

    feat: he.decode 추가

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ const fs = require('fs');
 const path = require('path');
 const { exec } = require('child_process');
 const {target} = require('./target');
+const he = require('he');
 
 const getFilePath = (filename)  => path.join(__dirname, `/generates/${filename}.txt`);
 
 const promises = target.map(({url, filename}) => {
-    return fetch(url).then(res => res.text()).then(decodeURI)
+    return fetch(url).then(res => res.text()).then(he.decode)
 })
 
 Promise.all(promises).then((htmlList) => {
