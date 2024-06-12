c16db888ea4d7e5eb1fb572dc718add70ad0de6c
miridih-jwlee01 <jwlee01@miridih.com>
2024-06-12 11:08:02 +0900

    refactor: index.js

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ const he = require('he');
 
 const getFilePath = (filename)  => path.join(__dirname, `/generates/${filename}.txt`);
 
-const promises = target.map(({url, filename}) => {
-    return fetch(url).then(res => res.text()).then(he.decode)
+const promises = target.map(async ({ url, filename }) => {
+    const html = await fetch(url).then(res => res.text()).then(he.decode)
+    return { html, path: getFilePath(filename)}
 })
 
-Promise.all(promises).then((htmlList) => {
-    htmlList.forEach((html, i) => {
+Promise.all(promises).then((res) => {
+    res.forEach(({ html, path }) => {
         const sortedMetas = html.match(/<meta[^>]*>/g)
             .sort()
             .map((meta) => meta.replace('/>', '>'))
-        fs.writeFileSync(getFilePath(target[i].filename), sortedMetas.join('\n'))
+
+        fs.writeFileSync(path, sortedMetas.join('\n'))
     })
 })
 
