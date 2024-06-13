b8836afaeb6787c66f7f082f601dc5287e911223
miridih-jwlee01 <jwlee01@miridih.com>
2024-06-13 10:15:46 +0900

    feat: getSeoTags로 네이밍 변경 및 canonical, alternate 가져올 수 있는 기능

--- lib.js
+++ lib.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
-const getMetaTags = (html) => html.match(/<meta[^>]*>/g);
+const getSeoTags = (html) => html.match(/<meta\s+[^>]*>|<link\s+[^>]*rel=["']?(canonical|alternate)["']?[^>]*>/gi);
 
 const replaceNoise = (str) => str.replace('/>', '>').replace(/\sid="[^"]*"/, '');
 
 const getFilePath = (filename)  => path.join(__dirname, `/generates/${filename}.txt`);
 
-module.exports = { getMetaTags, replaceNoise, getFilePath };
+module.exports = { getMetaTags: getSeoTags, replaceNoise, getFilePath };
